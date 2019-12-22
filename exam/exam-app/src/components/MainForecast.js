aad0c16240d0ec1cbc38f5420928d7f93aef7a01
Martin Komitsky <m.komitsky@corp.mail.ru>
2019-12-22 19:57:05 +0300

    exam: fix

--- exam/exam-app/src/components/MainForecast.js
+++ exam/exam-app/src/components/MainForecast.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../mainforecast.module.css';
-
 
 export default function MainForecast() {
 
@@ -22,52 +21,52 @@ export default function MainForecast() {
     const [toggle, setToggle] = useState(false);
 
     const scrollToBottom = () => {
-		myRef.current.scrollIntoView({ behavior: 'smooth', block: 'end'});
-	};
+        myRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    };
 
-	useEffect(scrollToBottom, [special]);
+    useEffect(scrollToBottom, [special]);
 
-  useEffect(() => {
+    useEffect(() => {
         const city_id = special.join();
         fetch(`https://api.openweathermap.org/data/2.5/group?id=${city_id}&units=metric&appid=b9af20ffd3ba40ecb7a0755286c703bc`)
-			.then(res => res.json())
-			.then(json => {
+            .then(res => res.json())
+            .then(json => {
                 console.log(json.list);
                 setForecasts(json.list);
                 return json
             })
-        }, [special]);
+    }, [special]);
 
 
     const WeatherPicture = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`
 
     const ForecastResult = () => {
-        const data = forecasts.map((city) => 
-        <Link className={styles.links} to = {`/${city.name}/`} key={city.id}>
-            <div className={styles.ForecastBox}>
-                <div className={styles.TopBox}>
-                    <div className={styles.CityName}>
-                        {city.name}
+        const data = forecasts.map((city) =>
+            <Link className={styles.links} to={`/${city.name}/`} key={city.id}>
+                <div className={styles.ForecastBox}>
+                    <div className={styles.TopBox}>
+                        <div className={styles.CityName}>
+                            {city.name}
+                        </div>
+                        <div className={styles.Temp}>
+                            <img alt='weather' src={WeatherPicture(city.weather[0].icon)} />
+                            {Math.round(city.main.temp)}<sup> o</sup>C
                     </div>
-                    <div className={styles.Temp}>
-                        <img alt='weather' src={WeatherPicture(city.weather[0].icon)}/>
-                        {Math.round(city.main.temp)}<sup> o</sup>C
+                    </div>
+                    <div className={styles.BottomBox}>
+                        <div className={styles.ExtendInfo}>
+                            {`Humidity ${city.main.humidity} | ${city.wind.speed} m/s`}
+                        </div>
+                        <div className={styles.MaxMinTemp}>
+                            {Math.round(city.main.temp_max)}<sup> o</sup>C/{Math.round(city.main.temp_min)}<sup> o</sup>C
+                    </div>
                     </div>
                 </div>
-                <div className={styles.BottomBox}>
-                    <div className={styles.ExtendInfo}>
-                        {`Humidity ${city.main.humidity} | ${city.wind.speed} m/s`}
-                    </div>
-                    <div className={styles.MaxMinTemp}>
-                        {Math.round(city.main.temp_max)}<sup> o</sup>C/{Math.round(city.main.temp_min)}<sup> o</sup>C
-                    </div>
-                </div>
-            </div>
-        </Link>
+            </Link>
         );
-        
+
         return (
-            <div className={styles.Result} ref = {myRef}>
+            <div className={styles.Result} ref={myRef}>
                 {data}
             </div>
         )
@@ -85,8 +84,8 @@ export default function MainForecast() {
     }
 
     const addUser = (event, city) => {
-		if (event.key === 'Enter') {
-			if (city !== '') {
+        if (event.key === 'Enter') {
+            if (city !== '') {
                 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b9af20ffd3ba40ecb7a0755286c703bc`)
                     .then(res => res.json())
                     .then(json => {
@@ -95,24 +94,24 @@ export default function MainForecast() {
                             ...special,
                             json.id
                         ])
-            })
-			}
-		}
-	};
+                    })
+            }
+        }
+    };
 
     const CreateInput = () => {
-		const [newcity, setNewCity] = useState('');
-		return (
-			<input
-				className = {styles.create_chat}
-				type='text'
-				value={newcity}
-				onChange={(event) => setNewCity(event.target.value)}
-				onKeyPress={(event) => {addUser(event, newcity.trim());}}
-			/>
-		); 
+        const [newcity, setNewCity] = useState('');
+        return (
+            <input
+                className={styles.create_chat}
+                type='text'
+                value={newcity}
+                onChange={(event) => setNewCity(event.target.value)}
+                onKeyPress={(event) => { addUser(event, newcity.trim()); }}
+            />
+        );
     };
-    
+
     return (
         <div className={styles.main}>
             <HeaderForecast />
@@ -120,14 +119,15 @@ export default function MainForecast() {
                 <ForecastResult />
             </div>
             <div className={styles.Btn}
-                role = 'button'
-                onClick = {() => {
-                    setToggle(!toggle)}}
-                onKeyPress = {() => {}}
-                tabIndex = '0'>
-                <img alt='add' src='https://image.flaticon.com/icons/png/512/149/149156.png'/>
+                role='button'
+                onClick={() => {
+                    setToggle(!toggle)
+                }}
+                onKeyPress={() => { }}
+                tabIndex='0'>
+                <img alt='add' src='https://image.flaticon.com/icons/png/512/149/149156.png' />
             </div>
             {toggle ? <CreateInput /> : null}
-        </div>  
-        )
-    }
+        </div>
+    )
+}
